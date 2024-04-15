6378ee6d86fbaa8f47280eafd7149dddf8ad79f3
Kristof Danko <krysstof@icloud.com>
2024-04-15 17:09:19 +1000

    remove commented lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header"
 import TaskBoard from "./components/TaskBoard"
@@ -6,23 +5,8 @@ import TaskBoard from "./components/TaskBoard"
 function App() {
   return (
     <div className="App">
-      
       <Header></Header>
       <TaskBoard></TaskBoard>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
